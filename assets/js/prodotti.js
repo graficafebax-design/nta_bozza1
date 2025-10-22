ce867a2fa86ad0402dfea1e5b40820c4546923c3
graficafebax-design <grafica.febax@gmail.com>
2025-10-22 16:41:24 +0200

    Update prodotti.js

--- assets/js/prodotti.js
+++ assets/js/prodotti.js
@@ -1,3 +1,4 @@
+  // SPC
 const prodotti = [
   {
     nome: "Positano",
@@ -13,7 +14,48 @@ const prodotti = [
     codice: "FRSPC0610180",
     categoria: "SPC"
   },
-  // altri prodotti...
+    {
+    nome: "Portofino",
+    link: "Portofino.html",
+    immagine: "assets/img/SPC/portofino/Portofino_03_03.jpg",
+    codice: "FRSPC0610181",
+    categoria: "SPC"
+  },
+    {
+    nome: "Nerano",
+    link: "Nerano.html",
+    immagine: "assets/img/SPC/nerano/Nerano_03_03.jpg",
+    codice: "FRSPC0610182",
+    categoria: "SPC"
+  },
+    {
+    nome: "Negrar",
+    link: "Negrar.html",
+    immagine: "assets/img/SPC/negrar/negrar_03_03.jpg",
+    codice: "FRSPC0610183",
+    categoria: "SPC"
+  },
+    {
+    nome: "Lindera",
+    link: "Lindera.html",
+    immagine: "assets/img/SPC/lindera/Lindera_03_03.jpg",
+    codice: "FRSPC0610184",
+    categoria: "SPC"
+  },
+    {
+    nome: "Concrete White",
+    link: "ConcreteWhite.html",
+    immagine: "assets/img/SPC/concrete white/concrete white_03_03.jpg",
+    codice: "FRSPC0610185",
+    categoria: "SPC"
+  },
+    {
+    nome: "Concrete Grey",
+    link: "ConcreteGrey.html",
+    immagine: "assets/img/SPC/concrete grey/concrete grey_03_03.jpg",
+    codice: "FRSPC0610185",
+    categoria: "SPC"
+  },
 ];
 
 
